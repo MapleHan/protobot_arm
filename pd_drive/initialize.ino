8054304ef9ae1d75fb0b318ee657ee37b0942698
MapleHan <190258039@qq.com>
2020-03-24 13:38:27 +0800

    修改减速比和端口定义

--- pd_drive/initialize.ino
+++ pd_drive/initialize.ino
@@ -19,10 +19,10 @@ void joint3Run()
 void armInit()
 {
   arm.getJoint(0).setName(kArmJointName[0]);
-  arm.getJoint(0).setMachineReduction(56.8);
+  arm.getJoint(0).setMachineReduction(53.325);
   arm.getJoint(0).setStepperOnestepAngleInDegree(0.087890625);
   arm.getJoint(0).setStepperSubdivision(1);
-  arm.getJoint(0).setMotorDirection(HIGH);
+  arm.getJoint(0).setMotorDirection(LOW);
   arm.getJoint(0).setLimitAngleSpeed(1.571);
   arm.getJoint(0).setStepperDEPPin(7,NULL_PIN,6);
   arm.getJoint(0).setTimer(&Timer1,joint0Run);
@@ -30,7 +30,7 @@ void armInit()
 
   arm.getJoint(1).setName(kArmJointName[1]);
   arm.getJoint(1).setLimitPosition(-M_PI/4,M_PI/4,true);
-  arm.getJoint(1).setMachineReduction(113.6);
+  arm.getJoint(1).setMachineReduction(110.205);
   arm.getJoint(1).setStepperOnestepAngleInDegree(0.087890625);
   arm.getJoint(1).setStepperSubdivision(1);
   arm.getJoint(1).setMotorDirection(HIGH);
@@ -41,7 +41,7 @@ void armInit()
 
   arm.getJoint(2).setName(kArmJointName[2]);
   //arm.getJoint(2).setLimitPosition(-M_PI/4.0*3.0,M_PI/4.0*3.0,true);
-  arm.getJoint(2).setMachineReduction(56.8);
+  arm.getJoint(2).setMachineReduction(53.325);
   arm.getJoint(2).setStepperOnestepAngleInDegree(0.087890625);
   arm.getJoint(2).setStepperSubdivision(1);
   arm.getJoint(2).setMotorDirection(HIGH);
@@ -51,12 +51,12 @@ void armInit()
   arm.getJoint(2).initialize();
 
   arm.getJoint(3).setName(kArmJointName[3]);
-  arm.getJoint(3).setMachineReduction(56.8);
+  arm.getJoint(3).setMachineReduction(12.638);
   arm.getJoint(3).setStepperOnestepAngleInDegree(0.087890625);
   arm.getJoint(3).setStepperSubdivision(1);
-  arm.getJoint(3).setMotorDirection(HIGH);
+  arm.getJoint(3).setMotorDirection(LOW);
   arm.getJoint(3).setLimitAngleSpeed(1.571);
-  arm.getJoint(3).setStepperDEPPin(35,NULL_PIN,34);
+  arm.getJoint(3).setStepperDEPPin(54,NULL_PIN,34);
   arm.getJoint(3).setTimer(&Timer8,joint3Run);
   arm.getJoint(3).initialize();     
 
